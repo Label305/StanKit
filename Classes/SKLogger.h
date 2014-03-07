7e096a04b2174be2bcf552359342566a44957880
Thijs Scheepers <thijs@label305.com>
2014-03-07 15:52:27 +0100

    Added some docs

--- Classes/SKLogger.h
+++ Classes/SKLogger.h
@@ -10,16 +10,50 @@
 #import <CrashlyticsFramework/Crashlytics.h>
 #import <TestFlightSDK/TestFlight.h>
 
+/**
+ Simple C-function to log something to the console and to the remote services.
+ */
 #define SKLog( s, ... ) [SKLogger logFor:__PRETTY_FUNCTION__ lineNumber:__LINE__ messageWithFormat:(s), ##__VA_ARGS__]
 
+/**
+ SKLogger is a supporting class to enable logging to several remote services.
+    
+    TestFlight - (only on setting the TESTING macro)
+    Crashlytics - (only on setting the DEBUG macro)
+ */
 @interface SKLogger : NSObject 
 
+/**
+ Convinience method that calls both setupTestFlightWithToken: and setupCrashlyticsWithAPIKey:
+ 
+ @see setupCrashlyticsWithAPIKey:
+ @see setupTestFlightWithToken:
+ */
 + (void)setupWithTestFlightToken:(NSString *)testFlightToken crashlyticsAPIKey:(NSString *)crashlyticsAPIKey;
 
+/**
+ Starts TestFlight aproriate to the application setup.
+ */
++ (void)setupTestFlightWithToken:(NSString *)testFlightToken;
+
+/**
+ Starts Crashlytics aproriate to the application setup.
+ */
++ (void)setupCrashlyticsWithAPIKey:(NSString *)crashlyticsAPIKey;
+
+/**
+ Logs a certain message to the console and remote services.
+ */
 + (void)logFor:(char *)methodDescription lineNumber:(NSInteger)lineNumber messageWithFormat :(NSString *)formatString, ...;
 
+/**
+ Call to pass a checkpoint and send it to TestFlight.
+ */
 + (void)passCheckpoint:(NSString *)string;
 
+/**
+ Call to set the users email for loggin purposes.
+ */
 + (void)setUserEmail:(NSString *)email;
 
 @end
