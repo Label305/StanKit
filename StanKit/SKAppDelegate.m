b368b52e3f02430e6cbee1f0fd86da59293ad813
Thijs Scheepers <tscheepers@users.noreply.github.com>
2014-03-31 16:48:59 +0200

    Update SKAppDelegate.m

--- StanKit/SKAppDelegate.m
+++ StanKit/SKAppDelegate.m
@@ -12,10 +12,6 @@
 #import "Analytics.h"
 
 
-#define kTestFlightToken @"41c0d390-8af5-47f9-a600-4beb1d94274c"
-#define kCrashlyticsAPIKey @"eb814520be52892fc29fc171c73b474b7e07603f"
-#define kGoogleAnalyticsTrackingId @"UA-34268655-4"
-
 
 @implementation SKAppDelegate
 
@@ -23,12 +19,12 @@
 {
     
 #ifdef DEBUG
-    [SKLogger setupWithMode:SKLoggerModeTesting testFlightToken:kTestFlightToken crashlyticsAPIKey:kCrashlyticsAPIKey];
+    [SKLogger setupWithMode:SKLoggerModeTesting testFlightToken:nil crashlyticsAPIKey:nil];
 #else
-    [SKLogger setupWithMode:SKLoggerModeDebug testFlightToken:kTestFlightToken crashlyticsAPIKey:kCrashlyticsAPIKey];
+    [SKLogger setupWithMode:SKLoggerModeDebug testFlightToken:nil crashlyticsAPIKey:nil];
 #endif
 
-    [SKAnalytics setupGoogleAnalyticsWithTrackingId:kGoogleAnalyticsTrackingId];
+    [SKAnalytics setupGoogleAnalyticsWithTrackingId:nil];
 
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
